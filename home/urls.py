a88ee0e37805d84ab0576ca559c9331289bfb504
Blackbeardpir8 <deepakdk15yadav.com>
2024-09-23 12:29:56 +0530

    all

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from home import views
 
 urlpatterns = [
-    path('', ),
+    path('', views.stockpicker, name = "stockpicker"),
+    path('stocktracker/', views.stocktracker, name = "stocktracker"),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from yahoo_fin.stock_info import *
 
 # Create your views here.
+def stockpicker(request):
+    stock_picker = tickers_nifty50()  
+    print(stock_picker)  
+    return render(request, "home/stockpicker.html", {"stockpicker": stock_picker})
+
+
+def stocktracker(request):
+    details = get_quote_table('RELIANCE.NS')
+    print(details)
+
+    # Passing the stock details to the template
+    context = {
+        'details': details
+    }
+    
+    return render(request, "home/stocktracker.html", context)
